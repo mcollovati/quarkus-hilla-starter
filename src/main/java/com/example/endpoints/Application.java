f0faea56cddc4069a42dcacfe9446d19d8fd64f4
Marco Collovati <mcollovati@gmail.com>
2026-01-02 17:36:26 +0100

    chore: use aura theme

--- src/main/java/com/example/endpoints/Application.java
+++ src/main/java/com/example/endpoints/Application.java
@@ -1,9 +1,10 @@
 package com.example.endpoints;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 
-@Theme(value = "quarkus-hilla-starter")
+@StyleSheet(value = "styles.css")
 public class Application implements AppShellConfigurator {
 
 }
